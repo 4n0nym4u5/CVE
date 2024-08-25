5cc865717b201637297bd4d660b840f9d08892e9
4n0nym4u5 <arjun1gu@gmail.com>
2024-08-25 10:40:19 +0530

    libheif crash

--- jbig2enc_poc/reproduce.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install libleptonica-dev
-cd jbig2enc
-./autogen.sh
-CFLAGS="-fsanitize=address -fno-omit-frame-pointer -g" CXXFLAGS=" -fsanitize=address -fno-omit-frame-pointer -g" ./configure --disable-shared
-make -j 12
-./src/jbig2 -s -S -p -v -d -2 -O ../poc/out.png ../poc/poc.jpg
